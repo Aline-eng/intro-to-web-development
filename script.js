2694a610224acd1d142a1726168e1b2960a0d932
Aline <alinenzikwinkunda@gmail.com>
2025-05-03 22:29:09 +0000

    Added About Me, Goal section, navbar links and dynmaic year

---
+++ script.js
@@ -0,0 +1,14 @@
+
+window.onload = function () {
+  function changeBackgroundColor(color) {
+    let section = document.getElementById('coding-journey');
+    section.style.backgroundColor = color;
+  }
+
+  document.getElementById('intro').addEventListener('click', () => changeBackgroundColor("#2CA02C"));
+  document.getElementById('react').addEventListener('click', () => changeBackgroundColor("#FF7F0E"));
+  document.getElementById('backend').addEventListener('click', () => changeBackgroundColor("#9467BD"));
+
+  // Safely set the year in the footer
+  document.getElementById('year').textContent = new Date().getFullYear();
+};
